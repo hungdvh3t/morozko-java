95519e50ccb816688e2df88aa484a90c7b086434
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2010-07-09 17:03:49 +0000

    Initial Import

---
+++ trunk/org.morozko.java.mod.codegen/src/org/morozko/java/mod/codegen/ConfigParse.java
@@ -0,0 +1,107 @@
+package org.morozko.java.mod.codegen;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.morozko.java.core.cfg.ConfigException;
+import org.morozko.java.core.xml.config.PropertyXML;
+import org.morozko.java.core.xml.dom.DOMUtils;
+import org.morozko.java.core.xml.dom.SearchDOM;
+import org.morozko.java.mod.daogen.gen.config.ConfigParser;
+import org.morozko.java.mod.daogen.gen.config.DGConfig;
+import org.morozko.java.mod.daogen.gen.config.FieldConfig;
+import org.morozko.java.mod.daogen.gen.config.TableConfig;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ConfigParse {
+	
+	private static void createMap( Element node, NavNode parent, NavMap navMap ) throws ConfigException {
+		NodeList nl = node.getChildNodes();
+		for ( int k=0; k<nl.getLength(); k++ ) {
+			Node current = nl.item( k );
+			if ( current.getNodeType() == Element.ELEMENT_NODE ) {
+				Element currentKid = ((Element)current);
+				if ( currentKid.getNodeName().equals( "nav-node" ) ) {
+					Properties props = DOMUtils.attributesToProperties( currentKid );
+					NavNode currentNode = new NavNode();
+					currentNode.setName( props.getProperty( "name" ) );
+					currentNode.setModule( props.getProperty( "module" ) );
+					currentNode.setDescription( props.getProperty( "description" ) );
+					currentNode.setFacade( props.getProperty( "facade" ) );
+					currentNode.setOverwrite( props.getProperty( "overwrite" ) );
+					currentNode.setOperation( props.getProperty( "operation" ) );
+					currentNode.setJspInclude( props.getProperty( "jsp-include" ) );
+					currentNode.setAuth( props.getProperty( "auth", "NOCHECK" ) );
+					String formName = props.getProperty( "form" );
+					if ( formName != null ) {
+						NavForm navForm = navMap.getFormMap().get( formName );
+						if ( navForm == null ) {
+							throw new ConfigException( "Form "+formName+" doesn't exist" );
+						}
+						currentNode.setForm( navForm );
+						currentNode.setFormInput( props.getProperty( "form-input" ) );
+						currentNode.setFormValidate( "true".equalsIgnoreCase( props.getProperty( "form-validate" ) ) );
+						System.out.println( "current form : "+currentNode.getName()+" : "+currentNode.isFormValidate()+" : "+currentNode.getFormInput() );
+					}
+					parent.getKids().add( currentNode );
+					createMap( currentKid , currentNode, navMap );
+				}
+			}
+		}
+	}
+	
+	public static NavMap parse( Element root ) throws Exception {
+		NavMap navMap = new NavMap();
+		SearchDOM searchDOM = SearchDOM.newInstance( true , true );
+		if ( root.getNodeName().equals( "codegen-config" ) ) {
+			// general props
+			Element generalProps = searchDOM.findTag( root , "general-props" );
+			navMap.setGeneralProps( PropertyXML.parse( generalProps ) );
+			
+			DGConfig dgConfig = null;
+			
+			// plugin doagen
+			String daogenPlugin = navMap.getGeneralProps().get( "daogen-plugin" );
+			if ( daogenPlugin != null ) {
+				ConfigParser cp = new ConfigParser();
+				dgConfig = cp.parseConfig( daogenPlugin );
+				navMap.setDaogen( dgConfig );	
+			}
+			
+			
+			// nav forms
+			Element navFormsTag = searchDOM.findTag( root , "nav-forms" );
+			if ( navFormsTag != null ) {
+				List<Element> navFormList = searchDOM.findAllTags( navFormsTag, "nav-form" );
+				Iterator<Element> navFormIt = navFormList.iterator();
+				while ( navFormIt.hasNext() ) {
+					Element navFormTag = navFormIt.next();
+					String name = navFormTag.getAttribute( "name" );
+					NavForm navForm = NavForm.newNavForm( name );
+					navMap.getFormMap().put( name , navForm );
+					String daogen = navFormTag.getAttribute( "daogen" );
+					if ( daogen != null ) {
+						navForm.setDaogen( daogen );
+						TableConfig formTable = dgConfig.getTable( daogen );
+						Iterator<FieldConfig> fields = formTable.getFields().iterator();
+						while ( fields.hasNext() ) {
+							FieldConfig fieldConfig = fields.next();
+							navForm.getFieldList().add( NavFormField.newField( fieldConfig.getJavaFieldName() , fieldConfig.getJavaFieldType() ) );
+						}
+					}
+				}
+			}
+			// nav map
+			Element navMapTag = searchDOM.findTag( root , "nav-map" );
+			createMap( navMapTag , navMap, navMap );
+		} else {
+			throw ( new Exception( "Root element must be codegen-config : "+root.getNodeName() ) );
+		}
+		return navMap;
+	}
+	
+	
+}
