6ffdb4d41f2d1a495211d72a3e64f8090de0feb4
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2010-10-25 19:25:08 +0000

    Info sistema

--- org.morozko.java.mod.dbsrc/src/org/morozko/java/mod/dbsrc/tool/DbsrcTool.java
+++ org.morozko.java.mod.dbsrc/src/org/morozko/java/mod/dbsrc/tool/DbsrcTool.java
@@ -2,7 +2,11 @@ package org.morozko.java.mod.dbsrc.tool;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.StringReader;
+import java.util.Properties;
 
+import org.morozko.java.core.io.FileIO;
+import org.morozko.java.core.text.regex.ParamFinder;
 import org.morozko.java.mod.dbsrc.config.ConfigurationData;
 import org.morozko.java.mod.dbsrc.config.Dbsrc;
 import org.morozko.java.mod.tools.util.args.Arg;
@@ -19,6 +23,7 @@ public class DbsrcTool {
 		System.out.println( "usage : DbsrcTool [options] " );
 		System.out.println( "	-c config-file" );
 		System.out.println( "	-o operation-name" );
+		System.out.println( "	-v key=value[;key=value]" );
 		System.out.println();
 	}
 	
@@ -38,10 +43,26 @@ public class DbsrcTool {
 				long starTime = System.currentTimeMillis();
 				String c = list.findArgValue( "c" );
 				String o = list.findArgValue( "o" );
-				File config = new File( c );
-				FileReader fr = new FileReader( config );
-				Dbsrc dbsrc = ConfigurationData.parseConfiguration( fr );
-				fr.close();
+				String v = list.findArgValue( "v" );
+				
+				String config = FileIO.readString( new File( c ) );
+				
+				if ( v != null ) {
+					ParamFinder finder = ParamFinder.newFinder();
+					Properties props = new Properties();
+					String[] vars = v.split( ";" );
+					for ( int k=0; k<vars.length; k++ ) {
+						String[] curr = vars[k].split( "=" );
+						props.setProperty( curr[0] , curr[1] );
+					}
+					config = finder.substitute( config , props );
+				}
+				
+				//System.out.println( "config >> "+config );
+				
+				StringReader sr = new StringReader( config );
+				Dbsrc dbsrc = ConfigurationData.parseConfiguration( sr );
+				sr.close();
 				dbsrc.exec( o );
 				long totalTime = (System.currentTimeMillis()-starTime);
 				System.out.println( "total exec time : "+(totalTime/1000)+"."+(totalTime%1000) );
