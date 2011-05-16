02ba2cd0e0ba203fd23e35a72fbb42691a2bc2f9
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-05-16 09:29:42 +0000

    Morozko lib

--- trunk/org.morozko.java.mod.navmap/src/org/morozko/java/mod/navmap/Version.java
+++ trunk/org.morozko.java.mod.navmap/src/org/morozko/java/mod/navmap/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
 	private final static String NUMBER = "1.0.0";
 	
-	private final static String DATE = "2010-05-21";
+	private final static String DATE = "2011-04-21";
 	
 	public static String getVersion() {
 		return NUMBER+" ("+DATE+")";
