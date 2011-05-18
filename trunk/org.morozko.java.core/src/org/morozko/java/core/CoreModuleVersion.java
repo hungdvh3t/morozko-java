251c6471c390384f708118088da1eb62ad6535eb
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-05-18 12:29:50 +0000

    Morozko Library

--- trunk/org.morozko.java.core/src/org/morozko/java/core/CoreModuleVersion.java
+++ trunk/org.morozko.java.core/src/org/morozko/java/core/CoreModuleVersion.java
@@ -5,8 +5,8 @@ import org.morozko.java.core.cfg.BaseModuleVersion;
 public class CoreModuleVersion extends BaseModuleVersion {
 
 	public static final String NAME = "Module Core";
-	public static final String VERSION = "1.0.2";
-	public static final String DATE = "2011-04-22";
+	public static final String VERSION = "1.0.3";
+	public static final String DATE = "2011-05-18";
 	public static final String DEPENDANCIES = "/org/morozko/java/core/dependancies.properties";
 	
 	public CoreModuleVersion() {
