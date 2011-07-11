8fe8ca0632fefbc558190af84a840aea9f694e72
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-07-11 12:55:23 +0000

    Library

--- org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/filter/DocRequestFacade.java
+++ org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/filter/DocRequestFacade.java
@@ -1,9 +1,13 @@
 package org.morozko.java.mod.doc.filter;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.morozko.java.core.ent.tld.html.HtmlTagUtils;
+import org.morozko.java.core.io.StreamIO;
 import org.morozko.java.core.lang.helpers.ClassHelper;
 import org.morozko.java.core.log.BasicLogObject;
 import org.morozko.java.core.text.DefaultTextFilter;
@@ -23,6 +28,7 @@ import org.morozko.java.core.xml.dom.DOMIO;
 import org.morozko.java.core.xml.dom.DOMUtils;
 import org.morozko.java.core.xml.dom.SearchDOM;
 import org.morozko.java.mod.doc.DocBase;
+import org.morozko.java.mod.doc.DocException;
 import org.morozko.java.mod.doc.DocFacade;
 import org.morozko.java.mod.doc.DocHelper;
 import org.morozko.java.mod.doc.config.DocConfig;
@@ -33,6 +39,8 @@ import org.w3c.dom.Element;
 
 public class DocRequestFacade extends BasicLogObject {
 
+	private static final DocHandler DEF = new DefaultDocHandler();
+	
 	public void handleDoc( HttpServletRequest request, HttpServletResponse response ) throws ServletException {
 		this.getLog().info( "start processing "+DocConfig.VERSION );
 		
@@ -77,27 +85,36 @@ public class DocRequestFacade extends BasicLogObject {
 		DocHandler docHandler = (DocHandler)this.docHandlerMap.get( name );
 		try {
 			
+			//se il doc handler è null usiamo il default doc handler
+			if ( docHandler == null ) {
+				this.getLog().info( "no doc handler found for document : "+name+", using default doc handler" );
+				docHandler = DEF;
+			}
+			
 			if ( docTypeHandler != null ) {
 				docTypeHandler.handleDocTypeInit(request, response, docContext);
 			}
-			
 			HttpServletResponse resp = null;
 			if ( "pushbody".equalsIgnoreCase( this.outMode ) ) {
 				resp = new HttpServletResponsePush( response );
 			} else {
 				resp = new HttpServletResponseByteData( response );
 			}
-					
 			docHandler.handleDoc( request, resp, this.context.getContext() );
-			
 			String encoding = docHandler.getEncoding();
-			
 			try {
 				response.setCharacterEncoding( encoding );	
 			} catch ( Throwable t  ) {
 				this.getLog().info( "failed setting character encoding : "+t );
 			}
-
+			
+			String jspRelative = this.jspPath+"/"+name+".jsp";
+			String jspHandler = this.context.getContext().getRealPath( jspRelative );
+			File jspFile = new File( jspHandler );
+			if ( !jspFile.exists() ) {
+				throw new DocException( "01", "Jsp File doesn't exists : '"+jspRelative+"'", null );
+			}
+			
 			String jspPath = this.jspPath+"/doc-handler.jsp";
 			if ( this.processingPage != null ) {
 				request.setAttribute( "doc-handler-name" , name );
@@ -106,6 +123,7 @@ public class DocRequestFacade extends BasicLogObject {
 			RequestDispatcher rd = request.getRequestDispatcher( jspPath );
 			this.getLog().info( "jspPath : '"+jspPath+"'" );
 			rd.forward( request , resp );
+
 			String xmlData = null;
 			if ( "pushbody".equalsIgnoreCase( this.outMode ) ) {
 				StringWriter sw = (StringWriter)request.getAttribute( "doc.writer" );
@@ -115,7 +133,6 @@ public class DocRequestFacade extends BasicLogObject {
 				re.flush();
 				xmlData = re.getBaos().toString();	
 			}
-			
 			if ( debug ) {
 				this.getLog().info( "xmlData 1 : \n"+xmlData );
 			}
@@ -130,53 +147,82 @@ public class DocRequestFacade extends BasicLogObject {
 				}
 			}		
 			
-
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			docContext.setXmlData( xmlData );
+			docContext.setType( type );
+			docContext.setContentType( contentType );
+			docContext.setEncoding( encoding );
 			
-			docContext.setXmlData(xmlData);
-			docContext.setBufferStream( baos );
+			this.handleDocWorker(request, response, docTypeHandler, docContext);
 			
-			if ( docTypeHandler == null ) {
-				DocBase docBase = DocFacade.parse( new StringReader( xmlData  ), this.docHelper );
-				String contentDisposition = null;
-				if ( type.equalsIgnoreCase( "xml" ) ) {
-					baos.write( xmlData.getBytes() );
-					contentDisposition = "attachment; filename="+name+".xml";
-				} else if ( type.equalsIgnoreCase( "pdf" ) ) {
-					DocFacade.createPDF( docBase , baos );
-					contentDisposition = "attachment; filename="+name+".pdf";
-				} else if ( type.equalsIgnoreCase( "pdf2" ) ) {
-					DocFacade.createPDFItext( docBase , baos );
-					contentDisposition = "attachment; filename="+name+".pdf";
-				} else if ( type.equalsIgnoreCase( "rtf" ) ) {
-					DocFacade.createRTF( docBase , baos );
-					contentDisposition = "attachment; filename="+name+".rtf";
-				} else if ( type.equalsIgnoreCase( "html" ) ) {
-					DocFacade.createHTML( docBase , baos );		
-					contentDisposition = "attachment; filename="+name+".html";
-				}
-				this.getLog().info("content type doc  : "+contentType+" : "+encoding );
-				this.getLog().info("contentDisposition  : "+contentDisposition );
-				if ( contentDisposition != null ) {
-					response.addHeader("Content-Disposition", contentDisposition );
-				}	
-			} else {
-				docTypeHandler.handleDocType(request, response, docContext);
-			}
-			
-			OutputStream os = response.getOutputStream();
-			baos.writeTo( os );
-			baos.flush();
-			os.flush();
 			docHandler.handleDocPost(request, response, context.getContext());
 			if ( docTypeHandler != null ) {
 				docTypeHandler.handleDocTypePost(request, response, docContext);
 			}
 		} catch (Exception e) {
-			this.getLog().error( "Error generating doc", e );
-			throw ( new ServletException( e ) );
+			if ( "document".equalsIgnoreCase( this.errorManager ) ) {
+				DocException de = null;
+				if ( e instanceof DocException ) {
+					de = (DocException)e;
+				} else {
+					de = new DocException( "99" , e.getMessage(), e );
+				}
+				ByteArrayOutputStream error = new ByteArrayOutputStream();
+				PrintStream errorStream = new PrintStream( error, true );
+				de.printStackTrace( errorStream );
+				errorStream.flush();
+				errorStream.close();
+				this.getLog().error( "Error generating doc", e );
+				Object[] args = { de.getCode(), de.getMessage(), error.toString() };
+				String xmlData = MessageFormat.format( ERROR_XML_DATA , args );
+				docContext.setXmlData( xmlData );
+				docContext.setType( type );
+				docContext.setContentType( contentType );
+				try {
+					handleDocWorker(request, response, docTypeHandler, docContext);
+				} catch (Exception e1) {
+					throw ( new ServletException( e1 ) );
+				}				
+			} else {
+				throw new ServletException( e );
+			}
 		}
 		this.getLog().info( "end processing" );
+	}
+	
+	private void handleDocWorker( HttpServletRequest request, HttpServletResponse response, DocTypeHandler docTypeHandler, DocContext docContext ) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		docContext.setBufferStream( baos );
+		if ( docTypeHandler == null ) {
+			DocBase docBase = DocFacade.parse( new StringReader( docContext.getXmlData()  ), this.docHelper );
+			String contentDisposition = null;
+			if ( docContext.getType().equalsIgnoreCase( "xml" ) ) {
+				docContext.getBufferStream().write( docContext.getXmlData().getBytes() );
+				contentDisposition = "attachment; filename="+docContext.getName()+".xml";
+			} else if ( docContext.getType().equalsIgnoreCase( "pdf" ) ) {
+				DocFacade.createPDF( docBase , docContext.getBufferStream() );
+				contentDisposition = "attachment; filename="+docContext.getName()+".pdf";
+			} else if ( docContext.getType().equalsIgnoreCase( "pdf2" ) ) {
+				DocFacade.createPDFItext( docBase , docContext.getBufferStream() );
+				contentDisposition = "attachment; filename="+docContext.getName()+".pdf";
+			} else if ( docContext.getType().equalsIgnoreCase( "rtf" ) ) {
+				DocFacade.createRTF( docBase , docContext.getBufferStream() );
+				contentDisposition = "attachment; filename="+docContext.getName()+".rtf";
+			} else if ( docContext.getType().equalsIgnoreCase( "html" ) ) {
+				DocFacade.createHTML( docBase , docContext.getBufferStream() );		
+				contentDisposition = "attachment; filename="+docContext.getName()+".html";
+			}
+			this.getLog().info("content type doc  : "+docContext.getContentType()+" : "+docContext.getEncoding() );
+			this.getLog().info("contentDisposition  : "+contentDisposition );
+			if ( contentDisposition != null ) {
+				response.addHeader("Content-Disposition", contentDisposition );
+			}	
+		} else {
+			docTypeHandler.handleDocType( request, response, docContext);
+		}
+		OutputStream os = response.getOutputStream();
+		baos.writeTo( os );
+		baos.flush();
+		os.flush();
 	}
 	
 	private void addTypeHandlers( SearchDOM searchDOM, Element rootTag ) {
@@ -212,6 +258,7 @@ public class DocRequestFacade extends BasicLogObject {
 			this.debug = Boolean.valueOf( generalConfigAtts.getProperty( "debug" ) ).booleanValue();
 			this.debug = Boolean.valueOf( generalConfigAtts.getProperty( "skip-filter" ) ).booleanValue();
 			this.processingPage = generalConfigAtts.getProperty( "processing-page" );
+			this.errorManager = generalConfigAtts.getProperty( "error-manager" );
 			this.getLog().info( "jsp-path : "+this.jspPath );
 			this.getLog().info( "debug    : "+this.debug );
 			this.getLog().info( "processing-page    : "+this.processingPage );
@@ -365,6 +412,8 @@ public class DocRequestFacade extends BasicLogObject {
 
 	private HashMap typeHandlerMap;
 	
+	private String errorManager;
+	
 	public String getOutMode() {
 		return outMode;
 	}
@@ -373,4 +422,17 @@ public class DocRequestFacade extends BasicLogObject {
 		this.outMode = outMode;
 	}
 	
+	private static String init() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			InputStream is = DocRequestFacade.class.getResourceAsStream( "/org/morozko/java/mod/doc/res/error-doc.xml" );
+			StreamIO.pipeStream( is , baos, StreamIO.MODE_CLOSE_BOTH );	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return baos.toString();
+	}
+	
+	private static final String ERROR_XML_DATA = init();
+		
 }
