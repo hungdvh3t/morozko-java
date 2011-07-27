c0e4d10af8e6c5abe4eadf226f034c83e0083392
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-07-27 08:20:52 +0000

    Library Fugerit

--- org.morozko.java.mod.web/src/org/morozko/java/mod/web/WebModuleVersion.java
+++ org.morozko.java.mod.web/src/org/morozko/java/mod/web/WebModuleVersion.java
@@ -5,8 +5,8 @@ import org.morozko.java.core.cfg.BaseModuleVersion;
 public class WebModuleVersion extends BaseModuleVersion {
 
 	public static final String NAME = "Module Web";
-	public static final String VERSION = "1.0.4";
-	public static final String DATE = "2011-07-11";
+	public static final String VERSION = "1.0.5";
+	public static final String DATE = "2011-07-27";
 	public static final String DEPENDANCIES = "/org/morozko/java/mod/db/dependancies.properties";
 	
 	public WebModuleVersion() {
