6f877013b4962467aa2faa2bf6466ecc8123b8d9
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-07-29 08:24:30 +0000

    Initial import.

--- org.morozko.java.mod.web/src/org/morozko/java/mod/web/actions/ActionProcessor.java
+++ org.morozko.java.mod.web/src/org/morozko/java/mod/web/actions/ActionProcessor.java
@@ -1,5 +1,7 @@
 package org.morozko.java.mod.web.actions;
 
+import java.io.Serializable;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,8 +11,15 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.morozko.java.core.log.LogFacade;
 
-public class ActionProcessor {
+public class ActionProcessor implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -773486825174878517L;
+
+	private ServletContext context;
+	
 	public static final String ATT_NAME = "org.morozko.java.mod.web.actions.ActionProcessor.ATT_NAME";
 	
 	public static final ActionProcessor DEFAULT_PROCESSOR = new ActionProcessor();
@@ -23,8 +32,17 @@ public class ActionProcessor {
 		return ap;
 	}
 	
+	public ServletContext getContext() {
+		return context;
+	}
+
+	public void setContext(ServletContext context) {
+		this.context = context;
+	}
+
 	public static void setProcessor( ServletContext context, ActionProcessor ap) {
 		context.setAttribute( ATT_NAME , ap );
+		ap.setContext( context );
 	}
 
 	
