4c6b4e2719cc45b1049b8b374ace7811df09b99d
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-08-04 21:34:22 +0000

    Util Web

--- util-web/src/util/UtilityServlet.java
+++ util-web/src/util/UtilityServlet.java
@@ -1,13 +1,7 @@
 package util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.sql.Timestamp;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -16,6 +10,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.handler.EditHandler;
+import util.handler.FileHandler;
+import util.handler.Util;
 
 /**
  * Servlet implementation class UtilityServlet
@@ -50,15 +48,15 @@ public class UtilityServlet extends HttpServlet {
         	if ( this.usePassword == null || currentUser != null ) {
         		String action = request.getParameter( "action" );
                 if ( action == null ) {
-                	handleFile(request, response, context);
+                	FileHandler.handleFile(request, response, context);
                 } else if ( "edit".equalsIgnoreCase( action ) ) {
-                	handleEdit(request, response, context);
+                	EditHandler.handleEdit(request, response, context);
                 }
         	} else {
         		handleUser(request, response, context);
         	}
         } catch(Exception e) {
-            PrintWriter pw = new PrintWriter(response.getWriter());
+            PrintWriter pw = Util.startHtml(response);
             pw.println("<pre>");
             e.printStackTrace(pw);
             pw.println("</pre>");
@@ -78,7 +76,7 @@ public class UtilityServlet extends HttpServlet {
 			}
 		} 
 		if ( !logged ) {
-			PrintWriter pw = new PrintWriter(response.getWriter());
+			PrintWriter pw = Util.startHtml(response);
 			pw.println("<html>");
 			pw.println("<body>");
 			pw.println("<form method='post' action='fs'>");
@@ -90,149 +88,7 @@ public class UtilityServlet extends HttpServlet {
 			pw.println("</html>");
 		}
 	}
-	
-	private static String fileAtts( File file ) {
-		StringBuffer atts = new StringBuffer();
-		if ( file.canRead() ) {
-			atts.append( "r" );
-		}
-		if ( file.canWrite() ) {
-			atts.append( "w" );
-		}
-		if ( file.canExecute() ) {
-			atts.append( "x" );
-		}
-		if ( file.isHidden() ) {
-			atts.append( "h" );
-		}
-		return atts.toString();
-	}
-	
-	private static String fileLabel( File file ) throws IOException {
-		StringBuffer label = new StringBuffer();
-		if ( file.getParentFile() != null ) {
-			label.append( file.getName() );
-			if ( file.isDirectory() && file.getParentFile() != null ) {
-				label.append( File.separator );
-			}			
-		} else {
-			label.append( file.getCanonicalPath() );
-		}
-		return label.toString();
-	}
-	
-	private static void handleFile( HttpServletRequest request, HttpServletResponse response, ServletContext context ) throws Exception {
-		// percorso da processare
-		String path = request.getParameter("path");
-		
-		// current file ed, eventualmente suo figli
-		File currentFile = null;
-		File[] listKids = null;
-		
-		// se il percorso non è impostato, allora parto dalle radici
-        if( path == null ) {
-        	listKids = File.listRoots();
-        // se il percorso è impostato allora uso il parametro
-        } else {
-        	currentFile = new File( path );
-        	if ( currentFile.isDirectory() ) {
-        		listKids = currentFile.listFiles();
-        	}
-        }
-			
-		if ( listKids != null ) {
-			PrintWriter pw = new PrintWriter(response.getWriter());
-			pw.println("<html>");
-			pw.println("<body>");
-			
-			if ( currentFile == null ) {
-				pw.println( "<h3>No path provided, listing file system roots : </h3>" );
-			} else {
-				String back = null;
-				if ( currentFile.getParentFile() != null ) {
-					back = "<a href='fs?path="+currentFile.getParentFile().getCanonicalPath()+"'>(..)</a>" ;	
-				} else {
-					back = "<a href='fs'>(..)</a></p>" ;
-				}
-				pw.println( "<h3>Directory listing for : "+currentFile.getCanonicalPath()+" "+back+"</h3>" );
-			}
-			
-			String tdSyle = " style='border: 1px solid black;' ";
-			
-			pw.println("<table style='width: 100%; border: 1px solid black;'>");
-			pw.println("<tr>");
-			pw.println("<th"+tdSyle+">Name</th>");
-			pw.println("<th"+tdSyle+">Size</th>");
-			pw.println("<th"+tdSyle+">Attributes</th>");
-			pw.println("<th"+tdSyle+">Last modified</th>");
-			pw.println("<th"+tdSyle+">Actions</th>");
-			pw.println("</tr>");
-			
-			for (int k = 0; k < listKids.length; k++) {
-				File current = listKids[k];
-				pw.println("<tr>");
-				pw.println("<td"+tdSyle+"><a href='fs?path="+current.getCanonicalPath()+"'>"+fileLabel( current )+"</a></td>");
-				pw.println("<td"+tdSyle+" align='right'>"+current.length()+"</td>");
-				pw.println("<td"+tdSyle+">"+fileAtts( current )+"</td>");
-				pw.println("<td"+tdSyle+" align='right'>"+new Timestamp( current.lastModified() )+"</td>");
-				pw.println("<td"+tdSyle+">&nbsp;" );
-				pw.println("<a target='editFrame' href='fs?action=edit&path="+current.getCanonicalPath()+"'>Edit</a>" );
-				pw.println("</td>" );
-				pw.println("</tr>");
-			}
-			
-			pw.println("</table>");
-			pw.println("</body>");
-			pw.println("</html>");
-		} else {
-			String contentDisposition = "attachment; filename="+currentFile.getName();
-			response.addHeader("Content-Disposition", contentDisposition);
-			byte buffer[] = new byte[1024];
-			FileInputStream fis = new FileInputStream(currentFile);
-			OutputStream os = response.getOutputStream();
-			for ( int read = fis.read(buffer); read > 0; read = fis.read(buffer) ) {
-				os.write( buffer, 0, read );
-			}
-			fis.close();
-		}
-	}
-	
-	private static void handleEdit( HttpServletRequest request, HttpServletResponse response, ServletContext context ) throws Exception {
-		
-		String path = request.getParameter( "path" );
-		File currentFile = new File( path );
-		String fileContent = null;
-		
-		if ( request.getParameter( "save" ) != null ) {
-			fileContent = request.getParameter( "fileContent" );
-			FileOutputStream fos = new FileOutputStream( currentFile );
-			fos.write( fileContent.getBytes() );
-			fos.close();
-		} else {
-			
-			byte buffer[] = new byte[1024];
-			FileInputStream fis = new FileInputStream(currentFile);
-			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			for ( int read = fis.read(buffer); read > 0; read = fis.read(buffer) ) {
-				os.write( buffer, 0, read );
-			}
-			fis.close();
-			fileContent = os.toString();			
-		}
-		
-		PrintWriter pw = new PrintWriter(response.getWriter());
-		pw.println("<html>");
-		pw.println("<body>");
-		pw.println("<form method='post' action='fs'>");
-		pw.println("<input type='hidden' name='action' value='edit'>");
-		pw.println("<input type='hidden' name='path' value='"+path+"'>");
-		pw.println("<textarea cols='120' rows='40' name='fileContent'>"+fileContent+"</textarea>");
-		pw.println("<input type='submit' name='save'>");
-		pw.println("</form>");
-		pw.println("</body>");
-		pw.println("</html>");
-	}
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
