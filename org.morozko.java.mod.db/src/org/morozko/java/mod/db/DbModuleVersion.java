fcc07bceaf509a8dd5159fc8ca7b235439a5f4ae
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-08-08 21:48:50 +0000

    Deneb

--- org.morozko.java.mod.db/src/org/morozko/java/mod/db/DbModuleVersion.java
+++ org.morozko.java.mod.db/src/org/morozko/java/mod/db/DbModuleVersion.java
@@ -5,8 +5,8 @@ import org.morozko.java.core.cfg.BaseModuleVersion;
 public class DbModuleVersion extends BaseModuleVersion {
 
 	public static final String NAME = "Module Db";
-	public static final String VERSION = "1.0.2";
-	public static final String DATE = "2011-08-05";
+	public static final String VERSION = "1.0.3";
+	public static final String DATE = "2011-08-08";
 	public static final String DEPENDANCIES = "/org/morozko/java/mod/db/dependancies.properties";
 	
 	public DbModuleVersion() {
