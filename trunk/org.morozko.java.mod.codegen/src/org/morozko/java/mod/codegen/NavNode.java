41ac6ad30e892b3edccf4546af3f3d0b8952c2d2
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-08-11 14:12:57 +0000

    Fugerit DB

--- trunk/org.morozko.java.mod.codegen/src/org/morozko/java/mod/codegen/NavNode.java
+++ trunk/org.morozko.java.mod.codegen/src/org/morozko/java/mod/codegen/NavNode.java
@@ -33,6 +33,37 @@ public class NavNode {
 	
 	private String auth;
 	
+	private String menu1;
+	
+	private String menu2;
+	
+	private String menu3;
+	
+	
+	public String getMenu1() {
+		return menu1;
+	}
+
+	public void setMenu1(String menu1) {
+		this.menu1 = menu1;
+	}
+
+	public String getMenu2() {
+		return menu2;
+	}
+
+	public void setMenu2(String menu2) {
+		this.menu2 = menu2;
+	}
+
+	public String getMenu3() {
+		return menu3;
+	}
+
+	public void setMenu3(String menu3) {
+		this.menu3 = menu3;
+	}
+
 	public String getAuth() {
 		return auth;
 	}
