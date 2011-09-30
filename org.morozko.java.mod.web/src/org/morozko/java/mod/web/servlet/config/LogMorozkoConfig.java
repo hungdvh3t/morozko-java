267be61487aaff3be656efee1ffc928d5ba3b903
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-09-30 18:53:08 +0000

    java lib

--- org.morozko.java.mod.web/src/org/morozko/java/mod/web/servlet/config/LogMorozkoConfig.java
+++ org.morozko.java.mod.web/src/org/morozko/java/mod/web/servlet/config/LogMorozkoConfig.java
@@ -8,6 +8,11 @@ import org.morozko.java.core.util.PropsIO;
 
 public class LogMorozkoConfig extends BasicConfig {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2775414520947736132L;
+
 	public void configure(Properties props) throws ConfigException {
 		this.getLog().info( "configure start" );
 		String configPath = props.getProperty( "log-config" );
