a7b6d41eb40654a6fa11e715db0f9b6162fe5f0c
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2011-11-07 14:53:52 +0000

    PMD

--- trunk/org.morozko.java.eclipse.fragment.pmd/src/org/morozko/java/mod/pmd/rules/struts/PublicAttributesInAction.java
+++ trunk/org.morozko.java.eclipse.fragment.pmd/src/org/morozko/java/mod/pmd/rules/struts/PublicAttributesInAction.java
@@ -1,9 +1,8 @@
 package org.morozko.java.mod.pmd.rules.struts;
 
-import java.util.List;
-
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.ast.ASTType;
+import net.sourceforge.pmd.ast.SimpleNode;
 import net.sourceforge.pmd.stat.StatisticalRule;
 
 
@@ -16,43 +15,53 @@ import net.sourceforge.pmd.stat.StatisticalRule;
  *
  */
 public class PublicAttributesInAction extends StatisticalRule {
-
-	private boolean isAction( Class c ) {
-		boolean ok = false;
-		String name = c.getName();
-		System.out.println( "CHECK NAME "+name );
-		if ( name.equals( "java.lang.Object" ) ) {
-			ok = false;
-		} else {
-			if ( name.equalsIgnoreCase( "org.apache.struts.Action" ) ) {
-				ok = true;
-			} else {
-				isAction( c.getSuperclass() );
-			}
-		}
-		return ok;
-	}
+	
+//	private static boolean isAction( Class c ) {
+//		boolean ok = false;
+//		if ( c != null ) {
+//			String name = c.getName();
+//			if ( name.equals( "java.lang.Object" ) ) {
+//				ok = false;
+//			} else {
+//				if ( name.equalsIgnoreCase( "org.apache.struts.Action" ) ) {
+//					ok = true;
+//				} else {
+//					isAction( c.getSuperclass() );
+//				}
+//			}			
+//		}
+//		return ok;
+//	}
 	
 	@Override
-	public Object visit(ASTClassOrInterfaceType node, Object data) {
-		Object result = null;
-		Class currentClass = node.getType();
-		System.out.println( "CHECK NAME 1 >>> "+currentClass.getName() );
-		return null;
+	public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+		try {
+			System.out.println( "TEST 1 a "+node );
+			System.out.println( "TEST 1 b "+data );
+			System.out.println( "TEST 1 c "+node.getType() );
+			for ( int k=0; k<node.jjtGetNumChildren(); k++ ) {
+				System.out.println( "TEST 1 a "+k+" - "+node.jjtGetChild( k ) );
+				try {
+					System.out.println( "TEST 2 b "+k+" - "+node.jjtGetChild( k ).getClass().getName() );	
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+			try {
+				ASTClassOrInterfaceType type = (ASTClassOrInterfaceType) ((SimpleNode) node.jjtGetChild(0)).jjtGetChild(0);
+	            Class clazz = type.getType();
+	            System.out.println( "TEST c : "+clazz.getName() );
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return super.visit(node, data);
 	}
 
-//	@Override
-//	public Object visit(ASTType node, Object data) {
-//		Object result = null;
-//		Class currentClass = node.getType();
-//		System.out.println( "CHECK NAME>>> "+currentClass.getName() );
-////		if ( this.isAction( currentClass ) ) {
-////			System.out.println( "IS ACTION! "+currentClass );
-////		}
-//		return result;
-//	}
-
 	public PublicAttributesInAction() {
+		System.out.println( "TEST INIT PublicAttributesInAction 2" );
 	}
 	
 }
