a78387088b5cf8261aef6d4491a19b41a94871dc
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2013-04-19 13:33:37 +0000

    Immagini

--- org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/DocModuleVersion.java
+++ org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/DocModuleVersion.java
@@ -5,8 +5,8 @@ import org.morozko.java.core.cfg.BaseModuleVersion;
 public class DocModuleVersion extends BaseModuleVersion {
 
 	public static final String NAME = "Module Doc";
-	public static final String VERSION = "1.2.8";
-	public static final String DATE = "2013-03-12";
+	public static final String VERSION = "1.2.9";
+	public static final String DATE = "2013-04-11";
 	public static final String DEPENDANCIES = "/org/morozko/java/mod/doc/dependancies.properties";
 	
 	public DocModuleVersion() {
