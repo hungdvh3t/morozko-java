a7f75d09f513d80dba232a1deaa9e089950613bf
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2013-10-12 09:38:54 +0000

    Morozko Doc

---
+++ trunk/org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/itext/v2/ITextHelper.java
@@ -0,0 +1,52 @@
+package org.morozko.java.mod.doc.itext.v2;
+
+import java.util.Properties;
+
+public class ITextHelper {
+
+	public ITextHelper() {
+		this.params = new Properties();
+	}
+	
+	private String defFontName;
+	
+	private String defFontSize;
+	
+	private String defFontStyle;
+		
+	private Properties params;
+
+	public Properties getParams() {
+		return params;
+	}
+
+	public void setParams(Properties params) {
+		this.params = params;
+	}
+
+	public String getDefFontName() {
+		return defFontName;
+	}
+
+	public void setDefFontName(String defFontName) {
+		this.defFontName = defFontName;
+	}
+
+	public String getDefFontSize() {
+		return defFontSize;
+	}
+
+	public void setDefFontSize(String defFontSize) {
+		this.defFontSize = defFontSize;
+	}
+
+	public String getDefFontStyle() {
+		return defFontStyle;
+	}
+
+	public void setDefFontStyle(String defFontStyle) {
+		this.defFontStyle = defFontStyle;
+	}
+	
+	
+}
