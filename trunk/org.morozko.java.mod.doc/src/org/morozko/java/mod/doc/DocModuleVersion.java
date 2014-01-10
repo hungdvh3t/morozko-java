ac3df4ce1c64e11590710d332385a215b717e604
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2014-01-10 10:53:57 +0000

    Handlers

--- trunk/org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/DocModuleVersion.java
+++ trunk/org.morozko.java.mod.doc/src/org/morozko/java/mod/doc/DocModuleVersion.java
@@ -5,8 +5,8 @@ import org.morozko.java.core.cfg.BaseModuleVersion;
 public class DocModuleVersion extends BaseModuleVersion {
 
 	public static final String NAME = "Module Doc";
-	public static final String VERSION = "1.3.1";
-	public static final String DATE = "2013-10-14";
+	public static final String VERSION = "1.3.2";
+	public static final String DATE = "2013-11-05";
 	public static final String DEPENDANCIES = "/org/morozko/java/mod/doc/dependancies.properties";
 	
 	public DocModuleVersion() {
