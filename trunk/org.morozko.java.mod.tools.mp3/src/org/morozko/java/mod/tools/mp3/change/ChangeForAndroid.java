02aacbd05828fbfc8d3714fc963fdfd0f254d682
fugerit.aetas <fugerit.aetas@ef7eca5e-5485-ef8e-9bef-ddb2dae84a27>
2014-07-02 19:56:29 +0000

    Librerie

--- trunk/org.morozko.java.mod.tools.mp3/src/org/morozko/java/mod/tools/mp3/change/ChangeForAndroid.java
+++ trunk/org.morozko.java.mod.tools.mp3/src/org/morozko/java/mod/tools/mp3/change/ChangeForAndroid.java
@@ -2,6 +2,7 @@ package org.morozko.java.mod.tools.mp3.change;
 
 import java.io.File;
 
+import org.farng.mp3.AbstractMP3Tag;
 import org.farng.mp3.MP3File;
 import org.farng.mp3.id3.AbstractID3v2;
 import org.farng.mp3.id3.ID3v1;
@@ -10,16 +11,31 @@ import org.morozko.java.mod.tools.util.args.ArgUtils;
 
 public class ChangeForAndroid {
 
-	public static void main( String[] args ) {
-		try {
-			ArgList argList = ArgUtils.parseArgs( args );
-			File folder = new File( argList.findArgValue( "dir" ) );
-			String remove = argList.findArgValue( "remove" );
-			boolean removeWhiteSpace = "true".equals( argList.findArgValue( "remove_white_space" ) );
-			boolean addTrackNumber = "true".equalsIgnoreCase( argList.findArgValue( "addTrackNumber" ) );
-			File[] list = folder.listFiles();
-			for ( int k=0; k<list.length; k++ ) {
-				File current = list[k];
+	private static Mp3TagChanger createTagChanger( MP3File mp3, String use ) {
+		Mp3TagChanger tagChanger = null;
+		if ( "id3v1".equalsIgnoreCase( use ) ) {
+			ID3v1 tc = mp3.getID3v1Tag();
+			tagChanger = Mp3TagChanger.newTagChanger( tc );
+		} else {
+			AbstractID3v2 tc = mp3.getID3v2Tag();
+			//AbstractMP3Tag tc = mp3.getID3v2Tag();
+			tagChanger = Mp3TagChanger.newTagChanger( tc );
+		}
+		return tagChanger;
+	}
+	
+	public static void recurseDir( File folder, ArgList argList ) throws Exception {
+		String dirFilter = argList.findArgValue( "dir-filter" );
+		String remove = argList.findArgValue( "remove" );
+		boolean removeWhiteSpace = "true".equals( argList.findArgValue( "remove_white_space" ) );
+		boolean addTrackNumber = "true".equalsIgnoreCase( argList.findArgValue( "addTrackNumber" ) );
+		File[] list = folder.listFiles();
+		for ( int k=0; k<list.length; k++ ) {
+			File current = list[k];
+			if ( current.isDirectory() && ( dirFilter == null || current.getName().contains( dirFilter ) ) ) {
+				recurseDir( current, argList );
+			} else {
+				
 				if ( current.getName().indexOf( ".mp3" ) != -1 ) {
 					System.out.println( "FILE : "+current.getCanonicalPath() );
 					// remove from file name
@@ -28,34 +44,39 @@ public class ChangeForAndroid {
 						File newFile = new File( folder, newFileName );
 						System.out.println( "RENAMING TO : "+newFile.getCanonicalPath() );
 						current.renameTo( newFile );
-						current = newFilMaggio2014
+						current = newFile;
 								
 					}
 					// remove white spaces
 					if ( removeWhiteSpace ) {
 						String newFileName = current.getName().replaceAll( " " , "_" );
 						File newFile = new File( folder, newFileName );
-						System.out.println( "RENAMING TO : "+newFile.getCanonicalPath() );
-						current.renameTo( newFile );
-						current = newFile;
+						if ( !newFile.getName().equals( current.getName() ) ) {
+							System.out.println( "RENAMING TO : "+newFile.getCanonicalPath() );
+							current.renameTo( newFile );
+							current = newFile;	
+						}
 					}
+					
 					// open as mp3
 					MP3File mp3 = new MP3File( current );
 					
-					String use = argList.findArgValue( "use" );
+					String useRead = argList.findArgValue( "read" );
+					String useWrite = argList.findArgValue( "write" );
 					
-					String trackNumber = null;
-					String songTitle = null;
-					boolean stateChange = false;
-					
-					if ( "id3v1".equalsIgnoreCase( use ) ) {
-						ID3v1 id3v1 = mp3.getID3v1Tag();
-						trackNumber = id3v1.getTrackNumberOnAlbum();
-						songTitle = id3v1.getSongTitle();
-					} else {
-						AbstractID3v2 id3v2 = mp3.getID3v2Tag();
-						trackNumber = id3v2.getTrackNumberOnAlbum();
-						songTitle = id3v2.getSongTitle();
+					if ( useRead != null ) {
+						String trackNumber = null;
+						String songTitle = null;
+						boolean stateChange = false;
+						
+						
+						Mp3TagChanger tagChangerRead = createTagChanger( mp3 , useRead );
+						Mp3TagChanger tagChangerWrite = createTagChanger( mp3 , useWrite );
+//						System.out.println( "tc read  : "+tagChangerRead );
+//						System.out.println( "tc write : "+tagChangerWrite );
+						
+						trackNumber = tagChangerRead.getTrackNumber();
+						songTitle = tagChangerRead.getSongTitle();
 //						// add track number to title
 						if ( addTrackNumber ) {
 							try {
@@ -63,31 +84,169 @@ public class ChangeForAndroid {
 								int tn = Integer.parseInt( trackNumber );
 								trackNumber = ("00"+tn);
 								trackNumber = trackNumber.substring( trackNumber.length()-2 );
-								if ( songTitle.startsWith( trackNumber+" - " ) ) {
+								if ( tagChangerWrite.getSongTitle().startsWith( trackNumber+" - " ) ) {
 									System.out.println( "Song title already starts with track number : "+trackNumber );
 								} else {
 									String newTitle = trackNumber+" - "+songTitle;
+									newTitle = new String( newTitle.getBytes(), "UTF-8" );
 									System.out.println( "adding track number to title : "+newTitle );
-									id3v2.setSongTitle( newTitle );
+									tagChangerWrite.setSongTitle( newTitle );
 									stateChange = true;	
 								}
 							} catch (NumberFormatException nfe) {
-								System.out.println( "NO VALID track number : "+nfe );
+								System.out.println( "NO VALID track number : "+nfe+" ("+songTitle+")" );
 							}						
 						}
+						
+						if ( stateChange ) {
+							tagChangerWrite.save( mp3 );
+						} else {
+							System.out.println( "track show "+trackNumber+" - "+songTitle.replace( " " , "" ) );
+						}
 					}
-					
-					if ( stateChange ) {
-						mp3.save();
-					} else {
-						System.out.println( "track "+trackNumber+" - "+songTitle );
-					}
+	
 				}
-
+				
 			}
+		}
+	}
+	
+	public static void main( String[] args ) {
+		try {
+			ArgList argList = ArgUtils.parseArgs( args );
+			File folder = new File( argList.findArgValue( "dir" ) );
+			recurseDir( folder, argList );
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+}
+
+abstract class Mp3TagChanger {
+	
+	public abstract void setSongTitle( String title );
+	
+	public abstract String getSongTitle();
+	
+	public abstract void setTrackNumber( String tn );
+	
+	public abstract String getTrackNumber();
+
+	public abstract void save( MP3File file ) throws Exception;
+	
+	public static Mp3TagChanger newTagChanger( ID3v1 tagChanger ) {
+		return new Idv3v1TagChanger( tagChanger );
+	}
+	
+//	public static Mp3TagChanger newTagChanger( AbstractMP3Tag tagChanger ) {
+//		return new Mp3RealTagChanger( tagChanger );
+//	}
+//	
+	
+	public static Mp3TagChanger newTagChanger( AbstractID3v2 tagChanger ) {
+		return new Idv3v2TagChanger( tagChanger );
+	}
+	
+}
+
+class Idv3v1TagChanger extends Mp3TagChanger {
+	
+	private ID3v1 tagChanger;
+
+	public Idv3v1TagChanger(ID3v1 tagChanger) {
+		super();
+		this.tagChanger = tagChanger;
+	}
+
+	public String getSongTitle() {
+		return tagChanger.getSongTitle();
+	}
+
+	public void setSongTitle(String arg0) {
+		tagChanger.setSongTitle(arg0);
+	}
+
+	public String getTrackNumber() {
+		return tagChanger.getTrackNumberOnAlbum();
+	}
+
+	public void setTrackNumber(String arg0) {
+		tagChanger.setTrackNumberOnAlbum(arg0);
+	}
+
+	@Override
+	public void save(MP3File file) throws Exception {
+		System.out.println( "SAVING id3v1 tags for file : "+file.getFilenameTag() );
+		file.setID3v1Tag( this.tagChanger );
+		file.save();
+	}
+	
+	
+	
+}
+
+class Idv3v2TagChanger extends Mp3TagChanger {
+	
+	private AbstractID3v2 tagChanger;
+
+	public Idv3v2TagChanger(AbstractID3v2 tagChanger) {
+		super();
+		this.tagChanger = tagChanger;
+	}
+
+	public String getSongTitle() {
+		return tagChanger.getSongTitle();
+	}
+
+	public void setSongTitle(String arg0) {
+		tagChanger.setSongTitle(arg0);
+	}
+	
+	public String getTrackNumber() {
+		return tagChanger.getTrackNumberOnAlbum();
+	}
+
+	public void setTrackNumber(String arg0) {
+		tagChanger.setTrackNumberOnAlbum(arg0);
+	}
+
+	@Override
+	public void save(MP3File file) throws Exception {
+		file.save();
+	}
+	
+}
+
+class Mp3RealTagChanger extends Mp3TagChanger {
+	
+	private AbstractMP3Tag tagChanger;
+
+	public Mp3RealTagChanger(AbstractMP3Tag tagChanger) {
+		super();
+		this.tagChanger = tagChanger;
+	}
+
+	public String getSongTitle() {
+		return tagChanger.getSongTitle();
+	}
+
+	public void setSongTitle(String arg0) {
+		tagChanger.setSongTitle(arg0);
+	}
+	
+	public String getTrackNumber() {
+		return tagChanger.getTrackNumberOnAlbum();
+	}
+
+	public void setTrackNumber(String arg0) {
+		tagChanger.setTrackNumberOnAlbum(arg0);
+	}
+
+	@Override
+	public void save(MP3File file) throws Exception {
+		file.setID3v2Tag( this.tagChanger );
+		file.save();
 	}
 	
 }
